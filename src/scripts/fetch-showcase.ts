06de387a93c45726c74f9a636e23aa7f8f276901
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2025-05-27 09:04:52 -0700

    Use CommonJS

--- src/scripts/fetch-showcase.ts
+++ src/scripts/fetch-showcase.ts
@@ -1,8 +1,10 @@
 import { fetchShinyShowcase } from '../utils/forumParser.js';
-import fs from 'fs/promises';
-import path from 'path';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
-const showcase = await fetchShinyShowcase();
-const dataPath = path.resolve('src/data/showcase.json');
-await fs.writeFile(dataPath, JSON.stringify(showcase, null, 2), 'utf-8');
-console.log('Showcase data written to /data/showcase.json');
+(async () => {
+    const showcase = await fetchShinyShowcase();
+    const dataPath = path.resolve('src/data/showcase.json');
+    await fs.writeFile(dataPath, JSON.stringify(showcase, null, 2), 'utf-8');
+    console.log('Showcase data written to /data/showcase.json');
+})();
