eb1f18fb6a819f0ca84294ff1c348e67c99c8db3
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2026-02-26 06:14:01 -0800

    Add utils for pokeapi

---
+++ packages/utils/pokeapi.js
@@ -0,0 +1,39 @@
+/** Fetches the national number for a given Pokémon name
+ * @param {string} pokemon - Pokémon name
+ * @returns {number|null} National number or null if not found
+ */
+async function getNationalNumber(pokemon) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (!response.ok) {
+      console.error(`Failed to fetch data for Pokémon "${pokemon}": ${response.statusText}`);
+    }
+    
+    const data = await response.json();
+    return data.id; // PokeAPI uses 'id' for national number
+  } catch (err) {
+    console.error(`Error fetching data for Pokémon "${pokemon}":`, err.message || err);
+  }
+}
+
+/**
+ * 
+ * @param {*} pokemonId national pokedex number of the pokemon
+ * @returns a URL to the Gen V animated shiny sprite associated with the pokemonId
+ */
+async function getSpriteUrl(pokemonId) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const data = await response.json();
+    return data.sprites.versions["generation-v"]["black-white"].animated.front_shiny;
+  } catch (err) {
+    console.error(`Error fetching data for Pokémon "${pokemonId}":`, err.message || err);
+    return null;
+  }
+}
+
+export {
+  getNationalNumber,
+  getSpriteUrl
+};
