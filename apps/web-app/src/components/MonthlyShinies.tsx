f21b09c9383507ab36f7c34d278f1106b64d992b
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2026-03-15 22:44:24 -0700

    Import pokeapi-js-wrapper

--- apps/web-app/src/components/MonthlyShinies.tsx
+++ apps/web-app/src/components/MonthlyShinies.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import MonthlyShiniesResults from './MonthlyShiniesResults';
+import { Pokedex } from 'pokeapi-js-wrapper';
+const P = new Pokedex();
 
 interface ShinyFromAPI {
   pokemon_name: string;
