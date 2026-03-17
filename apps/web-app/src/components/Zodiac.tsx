709af92748432539344d47bb4e44c5755e84066e
Tu-An Nguyen <t.nhunhu.ng@gmail.com>
2026-03-17 13:31:26 -0700

    Prepare zodiac page

--- apps/web-app/src/components/Zodiac.tsx
+++ apps/web-app/src/components/Zodiac.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Leaderboard from './Leaderboard';
+import zodiacData from "../data/zodiac.json";
 
 const Zodiac = () => {
   return (
@@ -9,21 +11,20 @@ const Zodiac = () => {
             SOJU Zodiac War
           </h2>
           <p className="text-gray-700 dark:text-gray-300 mb-8">
-            Join us for our second Zodiac Shiny War on March 20-29! Sign up in
-            the{" "}
-            <code className="font-mono bg-gray-100 dark:bg-gray-700 p-1 rounded">
-              #event-ideas💡
-            </code>{" "}
+            Welcome back to Team Soju's annual Zodiac Wars! Participants will be split into 3 teams and shiny hunt a list of eligible Lunar New Year themed Pokémon. Each shiny caught will be awarded points and the team with the most points by the end of the event will be crowned the winner. For more information on the rules and eligible Pokémon, check out the
             <a
-              href="../../discord"
+              href="https://forums.pokemmo.com/index.php?/topic/196794-team-soju-zodiac-wars-2026-lny-bounty-shiny-wars/"
               className="text-primary-600 dark:text-primary-400 hover:underline ml-1"
             >
-              Discord
-            </a>{" "}
-            channel. Stay tuned for more details.
+            forum post
+            </a>
+            .
           </p>
+          <img src="/images/2026/zodiac/zodiac-wars-banner.png" alt="Zodiac Wars Banner" className="w-full rounded-lg mb-12 block" />
         </div>
       </div>
+
+      <Leaderboard teams={zodiacData.teams} />
     </section>
   );
 };
